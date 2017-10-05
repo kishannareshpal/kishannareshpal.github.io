65f6aad354e9fc4196849dda9958802cc9831676
kishan <kishan_jadav@hotmail.com>
2017-10-05 11:55:09 +0200

    s

--- yes.js
+++ yes.js
@@ -12,6 +12,6 @@ function setup() {
 
 function draw() {
   ellipse(mouseX, mouseY, 80, 80);
-  even_smaller_text.innerHTML=accelerationX + "aX"; 
-  error_text.innerHTML=accelerationY + "aY";
+  even_smaller_text.innerHTML=accelerationX + " :aX";
+  error_text.innerHTML=accelerationY + " :aY";
 }

b9132669c0c0f39ceae3a4ae12b867c2eba1e958
kishan <kishan_jadav@hotmail.com>
2017-10-05 11:48:55 +0200

    s

--- yes.js
+++ yes.js
@@ -11,11 +11,6 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
   ellipse(mouseX, mouseY, 80, 80);
-  error_text.innerHTML=mouseX;
+  error_text.innerHTML=pAccelerationX;
 }

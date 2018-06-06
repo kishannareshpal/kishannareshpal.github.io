ed0a6cc28dc381a9ab335b4509bfed32de3db09c
Kishan Jadav <kishan_jadav@hotmail.com>
2018-06-07 00:01:59 +0200

    removed the test, added oficial land page

---
+++ main.js
@@ -0,0 +1,21 @@
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    name: 'basic-gradient',
+    direction: 'left-right', // 'diagonal', 'top-bottom', 'radial'
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#122036', '#043a61'],
+                ['#02AAB0', '#00CDAC'],
+                ['#22ff8f', '#e5ff7e'],
+                ['#fff022', '#ee7133'],
+                ['#31e97b', '#626262'],
+                ['#ffca6f', '#ff4545'],
+                ['#45d09a', '#740000'],
+            ],
+            transitionSpeed: 9500
+        }
+    }
+});

693967e32ddeb040458e20db07e7b515e756d691
Kishan Jadav <kishan_jadav@hotmail.com>
2023-12-24 09:06:17 +0000

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { NextUIProvider } from '@nextui-org/react';
-import { ThemeProvider as NextThemeProvider } from 'next-themes';
 
 export const Providers = ({children}: { children: React.ReactNode }) => {
 

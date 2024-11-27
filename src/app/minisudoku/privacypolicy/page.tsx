4ba7ac9ff6fd4e7dd94b8bedf8dd177082cddb78
Kishan Jadav <kishan_jadav@hotmail.com>
2024-11-27 16:54:23 +0000

    Add minisudoku/privacypolicy

---
+++ src/app/minisudoku/privacypolicy/page.tsx
@@ -0,0 +1,177 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className='flex flex-col gap-12 p-12'>
+      <div className="flex flex-col gap-5">
+        <h1 className='text-4xl font-black text-primary'>Privacy Policy</h1>
+        <h4 className="font-bold my-5">Last updated 27th November 2024</h4>
+
+        <p>
+          This privacy notice for Kishan Jadav (“we,” “us,” or “our“), describes how and why we might collect, store, use, and/or share (“process“) your information when you use our services (“Services“), such as when you:
+        </p>
+
+        <ul className="list-disc ps-8">
+          <li>Download and use our Apple Watch application (MiniSudoku for Watch), or any other application of ours that links to this Privacy Notice.</li>
+          <li>Engage with us in other related ways, including any sales, marketing, or events.</li>
+        </ul>
+
+        <p>
+          <strong>Questions or concerns?</strong> Reading this privacy notice will help you understand your privacy rights and choices. If you do not agree with our policies and practices, please do not use our Services. If you still have any questions or concerns, please contact us at <a className="text-primary" href="mailto:privacy@kishanjadav.com">privacy@kishanjadav.com</a>.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-5">
+        <h2 className='text-xl font-semibold'>Summary of key points</h2>
+        <p>
+          This summary provides key points from our privacy notice, but you can find out more details about any of these topics by using our table of contents below to find the section you are looking for.
+        </p>
+
+        <p>
+          <strong>What personal information do we process?</strong> When you visit, use, or navigate our Services, we may process personal information depending on how you interact with Kishan Jadav and the Services, the choices you make, and the products and features you use.
+        </p>
+
+        <p>
+          <strong>Do we process any sensitive personal information?</strong> We do not process sensitive personal information.
+        </p>
+
+        <p>
+          <strong>Do you receive any information from third parties?</strong> We may receive information from public databases, marketing partners, social media platforms, and other outside sources.
+        </p>
+
+        <p>
+          <strong>How do you process my information?</strong> We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent. We process your information only when we have a valid legal reason to do so.
+        </p>
+
+        <p>
+          <strong>In what situations and with which types of parties do we share personal information?</strong> We may share information in specific situations and with specific categories of third parties.
+        </p>
+
+        <p>
+          <strong>How do we keep your information safe?</strong> We have organizational and technical processes and procedures in place to protect your personal information. However, no electronic transmission over the internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other unauthorized third parties will not be able to defeat our security and improperly collect, access, steal, or modify your information.
+        </p>
+
+        <p>
+          <strong>What are your rights?</strong> Depending on where you are located geographically, the applicable privacy law may mean you have certain rights regarding your personal information.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-5">
+        <h2 className='text-xl font-semibold mb-2'>Table of contents</h2>
+
+        <ul className="flex flex-col gap-2 ps-8">
+          <li>
+            <a className="text-primary underline" href="#information-we-collect">1. WHAT INFORMATION DO WE COLLECT?</a>
+          </li>
+          <li>
+            <a className="text-primary underline" href="#stance-on-third-party-websites">2. WHAT IS OUR STANCE ON THIRD-PARTY WEBSITES?</a>
+          </li>
+          <li>
+            <a className="text-primary underline" href="#cookies">3. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</a>
+          </li>
+          <li>
+            <a className="text-primary underline" href="#privacy-rights">4. WHAT ARE YOUR PRIVACY RIGHTS?</a>
+          </li>
+          <li>
+            <a className="text-primary underline" href="#updates-to-notice">5. DO WE MAKE UPDATES TO THIS NOTICE?</a>
+          </li>
+          <li>
+            <a className="text-primary underline" href="#contact-us">6. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</a>
+          </li>
+          <li>
+            <a className="text-primary underline" href="#review-your-data">7. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?</a>
+          </li>
+        </ul>
+      </div>
+
+      <div id="information-we-collect" className="flex flex-col gap-5">
+        <h2 className='text-xl font-semibold mb-2'>1. WHAT INFORMATION DO WE COLLECT?</h2>
+
+        <h4>Personal information you disclose to us</h4>
+
+        <p className="italic">
+          <strong>In Short:</strong> We do not collect any of your personal information.
+        </p>
+
+        <p>
+          <strong>Sensitive information:</strong> We do not collect any of your personal information.
+        </p>
+      </div>
+
+      <div id="stance-on-third-party-websites" className="flex flex-col gap-5">
+        <h2 className='text-xl font-semibold mb-2'>2. WHAT IS OUR STANCE ON THIRD-PARTY WEBSITES?</h2>
+
+        <p className="italic">
+          <strong>In Short:</strong> We are not responsible for the safety of any information that you share with third parties that we may link to or who advertise on our Services, but are not affiliated with, our Services.
+        </p>
+
+        <p>
+          The Services, may link to third-party websites, online services, or mobile applications and/or contain advertisements from third parties that are not affiliated with us and which may link to other websites, services, or applications. Accordingly, we do not make any guarantee regarding any such third parties, and we will not be liable for any loss or damage caused by the use of such third-party websites, services, or applications. The inclusion of a link towards a third-party website, service, or application does not imply an endorsement by us. We cannot guarantee the safety and privacy of data you provide to any third parties. Any data collected by third parties is not covered by this privacy notice. We are not responsible for the content or privacy and security practices and policies of any third parties, including other websites, services, or applications that may be linked to or from the Services. You should review the policies of such third parties and contact them directly to respond to your questions.
+        </p>
+      </div>
+
+      <div id="cookies" className="flex flex-col gap-5">
+        <h2 className='text-xl font-semibold mb-2'>3. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</h2>
+
+        <p className="italic">
+          <strong>In Short:</strong> We may use cookies and other tracking technologies to collect and store your information.
+        </p>
+
+        <p>
+          We may use cookies and similar tracking technologies (like web beacons and pixels) to access or store information.
+        </p>
+      </div>
+
+      <div id="privacy-rights" className="flex flex-col gap-5">
+        <h2 className='text-xl font-semibold mb-2'>4. WHAT ARE YOUR PRIVACY RIGHTS?</h2>
+
+        <p className="italic">
+          <strong>In Short:</strong> In some regions, such as the European Economic Area (EEA), United Kingdom (UK), and Canada, you have rights that allow you greater access to and control over your personal information. You may review, change, or terminate your account at any time.
+        </p>
+
+        <p>
+          In some regions (like the EEA, UK, and Canada), you have certain rights under applicable data protection laws. These may include the right (i) to request access and obtain a copy of your personal information, (ii) to request rectification or erasure; (iii) to restrict the processing of your personal information; and (iv) if applicable, to data portability. In certain circumstances, you may also have the right to object to the processing of your personal information. You can make such a request by contacting us by using the contact details provided in the section “HOW CAN YOU CONTACT US ABOUT THIS NOTICE?” below.
+        </p>
+
+        <p>
+          We will consider and act upon any request in accordance with applicable data protection laws.
+        </p>
+
+        <p>
+          If you are located in the EEA or UK and you believe we are unlawfully processing your personal information, you also have the right to complain to your local data protection supervisory authority. You can find their contact details here: https://edpb.europa.eu/about-edpb/about-edpb/members_en.
+        </p>
+
+        <p>
+          If you are located in Switzerland, the contact details for the data protection authorities are available here: https://www.edoeb.admin.ch/edoeb/en/home.html.
+        </p>
+      </div>
+
+
+      <div id="updates-to-notice" className="flex flex-col gap-5">
+        <h2 className='text-xl font-semibold mb-2'>5. DO WE MAKE UPDATES TO THIS NOTICE?</h2>
+
+        <p className="italic">
+          <strong>In Short:</strong> Yes, we will update this notice as necessary to stay compliant with relevant laws.
+        </p>
+
+        <p>
+          We may update this privacy notice from time to time. The updated version will be indicated by an updated “Revised” date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy notice, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy notice frequently to be informed of how we are protecting your information.
+        </p>
+      </div>
+
+      <div id="contact-us" className="flex flex-col gap-5">
+        <h2 className='text-xl font-semibold mb-2'>6. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</h2>
+
+        <p>
+          If you have questions or comments about this notice, you may contact us by email at <a className="text-primary" href="mailto:privacy@kishanjadav.com">privacy@kishanjadav.com</a>.
+        </p>
+      </div>
+
+      <div id="review-your-data" className="flex flex-col gap-5">
+        <h2 className='text-xl font-semibold mb-2'>7. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?</h2>
+
+        <p>
+          Based on the applicable laws of your country, you may have the right to request access to the personal information we collect from you, change that information, or delete it in some circumstances. To request to review, update, or delete your personal information, please send us an email at <a className="text-primary" href="mailto:privacy@kishanjadav.com">privacy@kishanjadav.com</a>.
+        </p>
+      </div>
+    </div>
+  )
+}
